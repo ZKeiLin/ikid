aa5e4ad3a809207b5d47bdfc9aebff1e2da84eae
ZKeiLin <zlin2016@uw.edu>
2019-02-04 17:13:58 -0800

    Initial Commit

---
+++ ikid/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  ikid
+//
+//  Created by Zhiqi Lin on 2/4/19.
+//  Copyright © 2019 Zhiqi Lin. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ ikid/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  ikid
+//
+//  Created by Zhiqi Lin on 2/4/19.
+//  Copyright © 2019 Zhiqi Lin. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
